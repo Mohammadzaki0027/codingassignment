9a5179e419143c3f79502597b717a7430449748c
Mohammadzaki0027 <zaki7875@gmail.com>
2022-05-06 02:43:04 +0530

    file

---
+++ component/navbar.js
@@ -0,0 +1,16 @@
+function navbar(){
+    return` <h3>
+    <a href="./index.html">Homepage</a>
+</h3>
+<div id="container">
+
+ 
+
+  <h3><a href="./Reciepe1.html">DayReciepe</a></h3>
+  <h3><a href="./Reciepe1.html">TrendingReciepe</a></h3>
+  <h3><a href="./login.html">Login</a></h3>
+  <h3><a href="./signup.html">Singup</a></h3>
+
+</div>`
+}
+export default navbar
